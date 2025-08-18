aa85a7e3d348d3b9232a54d1e6a36535f43068fc
IslamRamzy444 <islamramzy2@gmail.com>
2025-08-18 23:08:21 +0300

    intialize empty project

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:graduation_project_movies/ui/onboarding/intro_screen.dart';
+import 'package:graduation_project_movies/utils/app_routes.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:graduation_project_movies/utils/app_theme.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRoutes.introScreenRouteName,
+      routes: {
+        AppRoutes.introScreenRouteName:(context)=>IntroScreen()
+      },
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      locale: Locale("en"),
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.dark,
+    );
+  }
+}
---
+++ lib/utils/app_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:graduation_project_movies/utils/app_colors.dart';
+
+class AppTheme {
+  static final ThemeData darkTheme=ThemeData(
+    scaffoldBackgroundColor: AppColors.blackColor,
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: AppColors.primaryColor,
+      iconTheme: IconThemeData(color: AppColors.primaryColor)
+    )
+  );
+}
