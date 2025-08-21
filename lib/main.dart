a113a6011c92dd9b3b2f6be650ca68a8cb1f90d7
Farouk-Ezz-ElArab <faroukezz9595@gmail.com>
2025-08-21 14:52:21 +0300

    construct cubit directory for cubits

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:graduation_project_movies/language_cubit/language_cubit.dart';
 import 'package:graduation_project_movies/ui/auth/login/login_screen.dart';
 import 'package:graduation_project_movies/ui/onboarding/intro_screen.dart';
 import 'package:graduation_project_movies/utils/app_routes.dart';
 //import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:graduation_project_movies/utils/app_theme.dart';
 
+import 'cubits/language_cubit/language_cubit.dart';
 import 'l10n/app_localizations.dart';
 
 void main() {
