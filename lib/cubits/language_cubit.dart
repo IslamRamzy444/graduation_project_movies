9af8424c014068013ade25650edeebea6e3fc409
IslamRamzy444 <islamramzy2@gmail.com>
2025-08-21 22:07:14 +0300

    implementing register ui page

--- lib/cubits/language_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:flutter/material.dart';
-
-class LanguageCubit extends Cubit<Locale> {
-  LanguageCubit() : super(const Locale('en'));
-
-  void changeLanguage(Locale locale) => emit(locale);
-}
