faf99769978a15339447e1cb7e9e2edab15f9949
IslamRamzy444 <islamramzy2@gmail.com>
2025-08-23 16:43:21 +0300

    solving some import conflicts

--- lib/ui/onboarding/cubit/onboarding_cubit.dart
+++ lib/ui/onboarding/cubit/onboarding_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:graduation_project_movies/ui/onboarding/cubit/onboarding_states.dart';
 import 'package:graduation_project_movies/utils/app_routes.dart';
 
