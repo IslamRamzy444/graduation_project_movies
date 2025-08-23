ca5b20cb54dcad95ae163b3024d34d6a1c379dbf
Farouk-Ezz-ElArab <faroukezz9595@gmail.com>
2025-08-23 17:58:42 +0300

    commit for next phase after getting the updated develop

--- lib/ui/auth/login/forget_password.dart
+++ lib/ui/auth/login/forget_password.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-//import 'package:graduation_project_movies/l10n/app_localizations.dart';
+//import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:graduation_project_movies/l10n/app_localizations.dart';
 import 'package:graduation_project_movies/ui/widgets/custom_Elevated_button.dart';
 import 'package:graduation_project_movies/ui/widgets/custom_text_form_field.dart';
 import 'package:graduation_project_movies/utils/app_assets.dart';
--- lib/ui/onboarding/onboarding_sixth_screen.dart
+++ lib/ui/onboarding/onboarding_sixth_screen.dart
@@ -2,8 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graduation_project_movies/ui/onboarding/cubit/onboarding_cubit.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-// import '../../l10n/app_localizations.dart';
+
+//import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import '../../l10n/app_localizations.dart';
 import '../../utils/app_assets.dart';
 import '../widgets/custom_onboarding_item.dart';
 
